c2fb9db01e8e3eee798ae44f19aee04701c0f3a7
Maxim Slysarenko <Coboom2002@gmail.com>
2024-08-01 21:23:17 +1000

    done

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const container = document.querySelector('.page');
+const cardList = container.querySelector('.places__list');
+
+function addCard(cardName, cardLink) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').setAttribute('src', cardLink);
+    cardElement.querySelector('.card__title').textContent = cardName;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+    cardList.append(cardElement);
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+initialCards.forEach((card) => {
+    addCard(card.name, card.link);
+});
