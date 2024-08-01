ededed5b9a6b05b73d44e471310c10e648006463
Maxim Slysarenko <Coboom2002@gmail.com>
2024-08-02 00:16:17 +1000

    pravki

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,35 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
 const container = document.querySelector('.page');
 const cardList = container.querySelector('.places__list');
 
-function addCard(cardName, cardLink) {
-    const cardTemplate = document.querySelector('#card-template').content;
+// @todo: Функция создания карточки
+function createCard(cardData, onDelete) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    cardElement.querySelector('.card__image').setAttribute('src', cardLink);
-    cardElement.querySelector('.card__title').textContent = cardName;
+    cardElement.querySelector('.card__image').setAttribute('src', cardData.link);
+    cardElement.querySelector('.card__image').setAttribute('alt', cardData.name);
+    cardElement.querySelector('.card__title').textContent = cardData.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+    deleteButton.addEventListener('click', () => onDelete(cardElement));
 
-    cardList.append(cardElement);
+    return cardElement;
 }
 
-function deleteCard(cardElement) {
+// @todo: Функция удаления карточки
+function handleDeleteCard(cardElement) {
     cardElement.remove();
 }
 
-initialCards.forEach((card) => {
-    addCard(card.name, card.link);
+// @todo: Вывести карточки на страницу
+function renderCard(cardElement) {
+    cardList.append(cardElement); // Можно использовать append или prepend
+}
+
+// Вывести все карточки на страницу
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData, handleDeleteCard);
+    renderCard(cardElement);
 });
